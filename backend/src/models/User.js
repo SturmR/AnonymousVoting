f618bee05d90396d087eb13b83d25f9e26b88c09
Onur Kafkas <onurkafkas2001@gmail.com>
2025-04-24 17:55:33 +0300

    Make password optional for anonymity.

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -6,7 +6,7 @@ const UserSchema = new mongoose.Schema({
     username: { type: String, required: true },
     isWatchlisted: { type: Boolean, default: false },
     email: { type: String, required: true },
-    password: { type: String, required: true },
+    password: { type: String, default: '' },
 });
 
 module.exports = mongoose.model('User', UserSchema);
